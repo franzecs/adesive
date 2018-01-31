59a95a1511082fcd216d5dfe8c07003cb7da5e2f
franzecs <franzecs@gmail.com>
2018-01-30 22:52:07 -0200

    ajustes deploy

--- src/main/java/com/ikytus/fp/util/security/UsuarioRepository.java
+++ src/main/java/com/ikytus/fp/util/security/UsuarioRepository.java
@@ -3,10 +3,12 @@ package com.ikytus.fp.util.security;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ikytus.fp.util.security.model.Grupo;
 import com.ikytus.fp.util.security.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
 	
 	public Usuario findByFuncional(String funcional);
--- src/main/resources/data2.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `grupo` (`codigo`,`descricao`,`nome`) VALUES (1,'usuário master','ROLE_MASTER');
-INSERT INTO `grupo` (`codigo`,`descricao`,`nome`) VALUES (2,'Clientes','ROLE_CLIENTES');
-INSERT INTO `grupo` (`codigo`,`descricao`,`nome`) VALUES (3,'Administradores locais','ROLE_ADMLOC');
-
-INSERT INTO `usuario` (`codigo`,`funcional`,`nome`,`senha`,`setor`) VALUES (1,'65795-6','FRANCISCO JOSÉ ','$2a$10$lZSTJbc3VWzRLHh2bSosdORNuE.pVR9oe/6I02wTHCM.IANuH7yAi','IKTUS');
-
-INSERT INTO `usuario_grupo` (`codigo`,`usuario_id`,`grupo_id`) VALUES (1,1,1);
